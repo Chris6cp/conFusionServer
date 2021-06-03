4a7e85443be8015dbfabe7433191ff1cdc99e76a
Chris Peter <peterchris@gmail.com>
2021-06-04 00:01:52 +0530

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+const { authenticate } = require('../models/user');
+var authen = require('../authenticate');
 
 router.use(bodyParser.json());
 router.get('/', function(req, res, next) {
@@ -30,12 +32,13 @@ router.post('/signup', (req, res, next) => {
 
 });
 
-router.post('/login', passport.authenticate('local'),(req, res) => {
+router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  var token = authen.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in'});
-  
-})
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+});
 
 router.get('/logout', (req, res) => {
   if (req.session) {
